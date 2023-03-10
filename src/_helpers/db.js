7d69df32c17bfb34910b8787b72ded58d87184de
amitkishorecemtrex <akishore@cemtrexlabs.com>
2023-03-10 10:41:31 +0530

    Update db.js

--- src/_helpers/db.js
+++ src/_helpers/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 //const MONGODB_URL = "mongodb://127.0.0.1:27017/ludo-api";
 const MONGODB_URL =
-  "mongodb+srv://amit1234:Nsit_delhi3@cluster0.d7rgz.mongodb.net/kho-api?retryWrites=true&w=majority";
+  "mongodb+srv://edendev:Y5oST9CKMlr2rVbS@edendev.2whynwg.mongodb.net/test";
 
 mongoose.connect(MONGODB_URL, {
   useUnifiedTopology: true,
