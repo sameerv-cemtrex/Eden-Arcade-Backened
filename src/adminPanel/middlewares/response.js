7b2f7839a6e267dc2ea8a0ce0652389b622d2839
ckanzarkar <ckanzarkar@cxr.agency>
2023-04-03 15:33:32 +0530

    response updated for all values in get endpoint

--- src/adminPanel/middlewares/response.js
+++ src/adminPanel/middlewares/response.js
@@ -5,10 +5,10 @@ const response = (model, populate) => {
     let modelsQuery = model.find();
 
     //convert query string to number
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.per_page) || 2;
-    const skip = (page - 1) * limit;
     const total = await model.countDocuments();
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.per_page) || total;
+    const skip = (page - 1) * limit;
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
     const sortBy = req.query.sort;
