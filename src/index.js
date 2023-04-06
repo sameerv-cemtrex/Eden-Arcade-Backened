f16d4522d9d4a83743d017427c6487fa37df8cca
amitkishorecemtrex <akishore@cemtrexlabs.com>
2023-04-06 10:35:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -67,9 +67,8 @@ app.use("/api/v1/admin-panel/tasks", TaskRouter);
 // app.use(notFoundError);
 app.use(globalErrorHandler);
 
-
-
 //var server2 = require("http").createServer(app);
+
  
   var https = require("https");
 var fs = require("fs");
@@ -84,7 +83,7 @@ var options = {
     "/etc/letsencrypt/live/eden-dev.cetxlabs.com-0002/chain.pem"
   ),
 };
-var server2 = https.createServer(options, app);  
+var server2 = https.createServer(options, app);   
 
 //TESTING IS SERVER RUNNING
 const server = server2.listen(port, () => {
